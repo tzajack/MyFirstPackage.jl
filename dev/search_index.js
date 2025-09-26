e247566f470d7db61c460835ae31d6b97ec81b32
Documenter.jl <documenter@juliadocs.github.io>
2025-09-26 02:43:58 +0000

    build based on 8dfd583

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MyFirstPackage","page":"Home","title":"MyFirstPackage","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyFirstPackage.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#MyFirstPackage.Point","page":"Home","title":"MyFirstPackage.Point","text":"Point{D, T}\n\nA point in D-dimensional space, with coordinates of type T.\n\n\n\n\n\n","category":"type"}]
+}
